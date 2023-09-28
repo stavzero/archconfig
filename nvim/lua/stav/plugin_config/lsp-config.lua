63acfaf540d1c7e02ede67ad8107e5655dfc63fd
stavzero <145792712+stavzero@users.noreply.github.com>
2023-09-28 16:08:40 +0300

    Update lsp-config.lua

--- nvim/lua/stav/plugin_config/lsp-config.lua
+++ nvim/lua/stav/plugin_config/lsp-config.lua
@@ -1,6 +1,6 @@
 require('mason').setup()
 require('mason-lspconfig').setup({
-	ensure_installed = { 'lua_ls' }
+	ensure_installed = {  }
 })
 
 local on_attach = function(_, _)
@@ -12,5 +12,3 @@ local on_attach = function(_, _)
 	vim.keymap.set('n', '<leader>gr', require('telescope.builtin').lsp_references, {})
 	vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 end
-
-require('lspconfig').sumneko_lua.setup {}
