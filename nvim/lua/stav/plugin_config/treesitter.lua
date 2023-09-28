6d6c83ec01bf9881ae55424bb9946f487c89546f
stavzero <145792712+stavzero@users.noreply.github.com>
2023-09-28 16:07:17 +0300

    Update treesitter.lua

--- nvim/lua/stav/plugin_config/treesitter.lua
+++ nvim/lua/stav/plugin_config/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
 	-- A list of parser names, or "all"
-	ensure_installed = { 'c', 'lua' },
+	ensure_installed = { 'c' },
 
 	-- Install parsers syncronously (only applied to 'ensure_installed')
 	sync_install = false,
